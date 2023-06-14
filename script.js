ef61f31b492c3ac7284ddcfeb22649f6a23e6f93
panzbyszek <woj.michalkiewicz@gmail.com>
2023-06-14 14:24:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $(window).on('scroll', function(){
 		$('header').removeClass('nav-show');
 	}
 })
-
+/*
 //hamburger
 const navSlide = () => {
 	 const hamburger = document.querySelector(".hamburger");
@@ -31,8 +31,70 @@ const navSlide = () => {
        hamburger.classList.toggle("toggle");
     }
 	}
+*/
+
+//hamburger
+const navSlide = () => {
+    const hamburger = document.querySelector(".hamburger");
+    const navbar = document.querySelector(".nav-bar");
+    const navLinks = document.querySelectorAll(".nav-bar li");
+    const speed = window.innerWidth <= 768 ? 0.3 : 0.5; // faster animation for mobile
+
+    hamburger.onclick = () => {
+        navbar.classList.toggle("nav-active");
+        
+        //Animation links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = "";
+            } 
+            else {
+                link.style.animation = `navLinkFade ${speed}s ease forwards ${index / 7+1}s`;
+            }
+        });
+        
+        //hamburger animation
+        hamburger.classList.toggle("toggle");
+    }
+
+    // hide navbar when click outside
+    document.onclick = (e) => {
+        if(e.target !== hamburger && e.target !== navbar && !navbar.contains(e.target)){
+            if(navbar.classList.contains("nav-active")){
+                navbar.classList.remove("nav-active");
+                hamburger.classList.toggle("toggle");
+
+                navLinks.forEach((link, index) => {
+                    if (link.style.animation) {
+                        link.style.animation = "";
+                    } 
+                });
+            }
+        }
+    }
+}
 
 window.onload = () => navSlide();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*//JS do przewijania projektow
 const container = document.querySelector('.card-container');
