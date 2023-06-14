5c34b2bb1c5c0a73c388d373a64d7ca1ea99c9b2
panzbyszek <woj.michalkiewicz@gmail.com>
2023-06-14 14:58:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ $(window).on('scroll', function(){
 		$('header').removeClass('nav-show');
 	}
 })
-/*
+
+
 //hamburger
 const navSlide = () => {
 	 const hamburger = document.querySelector(".hamburger");
@@ -24,55 +25,13 @@ const navSlide = () => {
 			link.style.animation = "";
 		} 
 		else {
-			link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7+1}s`;
+			link.style.animation = `navLinkFade 0.3s ease forwards ${index / 7+0.6}s`;
 		}
 	 });
        //hamburger animation
        hamburger.classList.toggle("toggle");
     }
 	}
-*/
-
-//hamburger
-const navSlide = () => {
-    const hamburger = document.querySelector(".hamburger");
-    const navbar = document.querySelector(".nav-bar");
-    const navLinks = document.querySelectorAll(".nav-bar li");
-    const speed = window.innerWidth <= 768 ? 0.3 : 0.5; // faster animation for mobile
-
-    hamburger.onclick = () => {
-        navbar.classList.toggle("nav-active");
-        
-        //Animation links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = "";
-            } 
-            else {
-                link.style.animation = `navLinkFade ${speed}s ease forwards ${index / 7+1}s`;
-            }
-        });
-        
-        //hamburger animation
-        hamburger.classList.toggle("toggle");
-    }
-
-    // hide navbar when click outside
-    document.onclick = (e) => {
-        if(e.target !== hamburger && e.target !== navbar && !navbar.contains(e.target)){
-            if(navbar.classList.contains("nav-active")){
-                navbar.classList.remove("nav-active");
-                hamburger.classList.toggle("toggle");
-
-                navLinks.forEach((link, index) => {
-                    if (link.style.animation) {
-                        link.style.animation = "";
-                    } 
-                });
-            }
-        }
-    }
-}
 
 window.onload = () => navSlide();
 
