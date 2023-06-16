a6173b9c1aa1c8685ec8df36d693849d8c989866
panzbyszek <woj.michalkiewicz@gmail.com>
2023-06-16 13:50:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -58,11 +58,44 @@ window.onload = () => navSlide();
 
 
 
+/*_______________________________________________________________________________*/
+/*                                  CRYPTO ALL                                   */
+/*_______________________________________________________________________________*/
+async function getAccount() {
+  if (window.ethereum) {
+    window.web3 = new Web3(window.ethereum);
+    try {
+      // Request account access if needed
+      await window.ethereum.enable();
+      // Acccounts now exposed
+      web3.eth.sendTransaction({/* ... */});
+    } catch (error) {
+      // User denied account access...
+      console.error("User denied account access")
+    }
+  }
+  // Legacy dapp browsers...
+  else if (window.web3) {
+    window.web3 = new Web3(web3.currentProvider);
+    // Acccounts always exposed
+    web3.eth.sendTransaction({/* ... */});
+  }
+  // Non-dapp browsers...
+  else {
+    console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+  }
+}
 
 
 
 
 
+
+
+
+/*_______________________________________________________________________________*/
+/*                                   NOT USED                                    */
+/*_______________________________________________________________________________*/
 /*
 //hamburger
 const navSlide = () => {
