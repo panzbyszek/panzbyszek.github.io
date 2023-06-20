7c75ef52bf2335e13d8049e1581428c359dacebb
panzbyszek <woj.michalkiewicz@gmail.com>
2023-06-20 13:42:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -95,7 +95,33 @@ async function disconnectWallet() {
     }
 }
 
+/*_______________________________________________________________________________*/
+/*                                Modal Projects                                 */
+/*_______________________________________________________________________________*/
 
+// Get the modal
+var modal = document.getElementById("modal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close-button")[0];
+
+// Function to open the modal and set the text
+function openModal(text) {
+  document.getElementById("modal-text").innerText = text;
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 
 
